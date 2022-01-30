d260aadcdbe66e48ef6633d62a8902070933016e
Swaye Chateau <swaye@kitechsoftware.com>
2022-01-30 06:11:58 -0500

    added language switcher

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,7 +1,9 @@
+import LanguageSwitcher from "./LanguageSwitcher"
+import { lang, langs } from './state'
 function App() {
   return (
     <div className="min-h-screen flex flex-col overflow-x-hidden bg-[#111]">
-      
+      <LanguageSwitcher show={false} absolute current={lang} langs={langs} onSwitch={()=> console.log}/>
     </div>
   )
 }
---
+++ react/src/state/index.ts
@@ -0,0 +1,51 @@
+export const lang = 'en-gb';
+export const langs = [
+  {
+    id: 1,
+    name: 'English (United Kingdom)',
+    native_name: 'English',
+    code: 'en-gb',
+  },
+  {
+    id: 2,
+    name: 'French (France)',
+    native_name: 'Français',
+    code: 'fr-fr',
+  },
+  {
+    id: 3,
+    name: 'Japanese',
+    native_name: '日本語',
+    code: 'ja',
+  },
+  {
+    id: 4,
+    name: 'Chinese (PRC)',
+    native_name: '中文',
+    code: 'zh-cn',
+  },
+  {
+    id: 5,
+    name: 'Welsh',
+    native_name: 'Cymraeg',
+    code: 'cy',
+  },
+  {
+    id: 6,
+    name: 'Korean (South Korea)',
+    native_name: '한국인',
+    code: 'ko-kr',
+  },
+  {
+    id: 7,
+    name: 'Spanish (Spain)',
+    native_name: 'Español',
+    code: 'es-es',
+  },
+  {
+    id: 8,
+    name: 'German (Germany)',
+    native_name: 'Deutsch',
+    code: 'de-de',
+  },
+];
