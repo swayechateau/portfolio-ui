076a797b28a485291545dbf2a1ce85211a8e1883
Swaye Chateau <swaye@kitechsoftware.com>
2022-01-30 06:35:15 -0500

    created footer

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,3 +1,4 @@
+import Footer from "./Footer"
 import LanguageSwitcher from "./LanguageSwitcher"
 import MobileNav from "./MobileNav"
 import { lang, langs } from './state'
@@ -6,6 +7,10 @@ function App() {
     <div className="min-h-screen flex flex-col overflow-x-hidden bg-[#111]">
       <LanguageSwitcher show={false} absolute current={lang} langs={langs} onSwitch={()=> console.log}/>
       <MobileNav yAxis={30}/>
+      <main className="grow">
+
+      </main>
+      <Footer />
     </div>
   )
 }
