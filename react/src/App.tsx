fe84602902942d590754047b796728205cc8de41
Swaye Chateau <swaye@kitechsoftware.com>
2022-01-30 06:32:01 -0500

    created mobile nav

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,9 +1,11 @@
 import LanguageSwitcher from "./LanguageSwitcher"
+import MobileNav from "./MobileNav"
 import { lang, langs } from './state'
 function App() {
   return (
     <div className="min-h-screen flex flex-col overflow-x-hidden bg-[#111]">
       <LanguageSwitcher show={false} absolute current={lang} langs={langs} onSwitch={()=> console.log}/>
+      <MobileNav yAxis={30}/>
     </div>
   )
 }
