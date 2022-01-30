d6b22da54d3c65708f47eef7dc83b9cbbdb18410
Swaye Chateau <swaye@kitechsoftware.com>
2022-01-30 06:58:32 -0500

    created desktop navigation

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,4 +1,5 @@
 import Banner from "./Banner"
+import DesktopNav from "./DesktopNav"
 import Footer from "./Footer"
 import LanguageSwitcher from "./LanguageSwitcher"
 import MobileNav from "./MobileNav"
@@ -8,6 +9,7 @@ function App() {
     <div className="min-h-screen flex flex-col overflow-x-hidden bg-[#111]">
       <LanguageSwitcher show={false} absolute current={lang} langs={langs} onSwitch={()=> console.log}/>
       <MobileNav yAxis={30}/>
+      <DesktopNav yAxis={30} />
       <main className="grow">
         <Banner
           large 
