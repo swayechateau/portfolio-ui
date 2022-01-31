0104750c73e128c1f2c59d5bcb27ca529fdf0729
Swaye Chateau <swaye@kitechsoftware.com>
2022-01-31 03:05:24 -0500

    created translate class

---
+++ react/src/translator/translate.ts
@@ -0,0 +1,63 @@
+class Translate {
+  model: any | null;
+  asArray: boolean = true;
+  defaultLanguage: string;
+  fallbackLanguage: string;
+  constructor(
+    model = null,
+    defaultLanguage = "en-gb",
+    fallbackLanguage = "en-gb"
+  ) {
+    this.fallbackLanguage = fallbackLanguage;
+    this.defaultLanguage = defaultLanguage;
+    this.model = model;
+  }
+
+  setModel(model: object[]): void {
+    this.model = model;
+  }
+
+  all(asArray:boolean = true): object[] {
+    this.asArray = asArray;
+    return this.asArray ? (this.all_as_array()) : (this.all_as_object());
+  }
+
+  all_as_array(): object[] {
+    let translatedModel: any = [];
+    this.get_keys().forEach((key) => {
+      translatedModel.push(this.one(key));
+    });
+    return translatedModel;
+  }
+
+  all_as_object(): object[] {
+    let translatedModel: any = {};
+    this.get_keys().forEach((key) => {
+      translatedModel[key] = this.one(key);
+    });
+    return translatedModel;
+  }
+
+  one(key: string | number): object | null {
+    if (this.check_key_exists(key)) {
+      return this.transform(this.model[key], key);
+    }
+    return null;
+  }
+
+  transform(data: any, index: any): object {
+    return data.translations[this.defaultLanguage]
+      ? data.translations[this.defaultLanguage]
+      : data.translations[this.fallbackLanguage];
+  }
+
+  get_keys(): string[] {
+    return Object.keys(this.model);
+  }
+
+  check_key_exists(key: string | number): boolean {
+    return this.model.hasOwnProperty(key);
+  }
+}
+
+export default Translate;
