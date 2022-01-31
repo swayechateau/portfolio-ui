dd9280423c7fde876afb284a27b9fec67a6d9a9c
Swaye Chateau <swaye@kitechsoftware.com>
2022-01-31 03:06:28 -0500

    created multi translate for pages

---
+++ react/src/translator/multi/multi-translate-page.ts
@@ -0,0 +1,35 @@
+import MultiTranslate from "./multi-translate";
+import {
+  IStatePage,
+  IStatePageTranslation,
+  ITranslatedPage,
+} from "../../interfaces/index";
+
+export class MultiTranslatePage extends MultiTranslate {
+  transform(data: IStatePage): object {
+    const dataTranslated: IStatePageTranslation =
+      data.translations[this.defaultLanguage] != null
+        ? data.translations[this.defaultLanguage]
+        : data.translations[this.fallbackLanguage];
+
+    const translated: ITranslatedPage = {
+      index: data.index,
+      published: data.published,
+      hero: data.hero,
+      slug: data.slug,
+      name: dataTranslated.name,
+      title: dataTranslated.title,
+      sub_title: dataTranslated.sub_title,
+      description: dataTranslated.description,
+      content: dataTranslated.content,
+      aside: dataTranslated.aside,
+      meta: dataTranslated.meta,
+      posted_at: data.posted_at,
+      updated_at: dataTranslated.updated_at,
+    };
+
+    return translated;
+  }
+}
+
+export default MultiTranslatePage;
