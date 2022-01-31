5f960808ebd0c0ad0085fc7d61b9717402665b43
Swaye Chateau <swaye@kitechsoftware.com>
2022-01-31 03:07:32 -0500

    added translate exporter index

---
+++ react/src/translator/index.ts
@@ -0,0 +1,13 @@
+import Translate from './translate';
+import MultiTranslate from './multi/multi-translate';
+import MultiTranslatePage from './multi/multi-translate-page';
+import MultiTranslatePost from './multi/multi-translate-post';
+import MultiTranslateProject from './multi/multi-translate-project';
+
+export {
+  Translate,
+  MultiTranslate,
+  MultiTranslatePage,
+  MultiTranslatePost,
+  MultiTranslateProject
+}
